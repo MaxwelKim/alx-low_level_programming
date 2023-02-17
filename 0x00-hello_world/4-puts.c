801ef4c927c237c7610df70918301d8e653f5fb3
MaxwelKim <mwiti.kimathi1796@gmail.com>
2023-02-16 18:17:42 -0800

    my first c code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,21 +1,11 @@
 #!/bin/bash
-
 #include <stdio.h>
-
 /**
- *
- * main - Aprogram that prints a line with puts function
- *
- * Return: 0
- *
- */
-
-int main(void) 
-
+* main - A program that prints a line with puts function
+* Return: 0 (success)
+*/
+int main(void)
 {
-
-	puts("Programming is like building a multilingual puzzle");
-	
-	return 0;
-
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
