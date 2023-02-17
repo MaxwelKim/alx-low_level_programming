c56b318184864cf369ee707b57ba357d0f7dca5b
MaxwelKim <mwiti.kimathi1796@gmail.com>
2023-02-16 17:36:04 -0800

    my first c code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,16 @@
 #!/bin/bash
-puts("Programming is like building a multilingual puzzle")
+#include <stdio.h>
+/**
+ * main - Aprogram that prints a line with puts function
+ * Return: 0
+ */
+
+int main(void) 
+
+{
+
+	puts("Programming is like building a multilingual puzzle");
+	
+	return 0;
+
+}
