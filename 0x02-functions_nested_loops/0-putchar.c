f8423113e1c16633350116ba88b10136836c24dc
MaxwelKim <mwiti.kimathi1796@gmail.com>
2023-02-22 12:54:42 -0800

    a program that prints _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,16 @@
 #include "main.h"
+#include "unistd.h"
 /**
  * main - print _putchar, followed by new line
  * Return: 0
  */
 int main(void);
 {
+#include <unistd.h>
+int _putchar(char c)
+{
+return (write(1, &c, 1));
+}
 _putchar('_');
 _putchar('p');
 _putchar('u');
