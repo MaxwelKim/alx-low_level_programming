33f7b98d2b38d0cc2e1d17388df012979da11cc7
MaxwelKim <mwiti.kimathi1796@gmail.com>
2023-02-27 22:03:09 +0300

    function that returns the length of a string

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -4,14 +4,13 @@
  * function that returns the length of a string
  * Return: int containing the converted number
  */
-int _strlen(char *s) 
+int _strlen(char *s)
 {
-    int len = 0;
-    while (*s != '\0') 
-    {
-        len++;
-        s++;
-    }
-    return len;
+int len = 0;
+while (*s != '\0')
+{
+len++
+s++;
 }
-
+return len;
+}
