83ca9d837252c93661f6170d951e08ec07888b6e
MaxwelKim <mwiti.kimathi1796@gmail.com>
2023-02-27 22:24:52 +0300

    function that returns the length of a string

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -4,6 +4,13 @@
  * Main-function that returns the length of a string
  * Return: int containing the converted number
  */
+int main(void)
+{
+    char str[] = "hello, world!";
+    int len = _strlen(str);
+    printf("The length of \"%s\" is %d.\n", str, len);
+    return 0;
+}
 int _strlen(char *s)
 {
 int len = 0;
