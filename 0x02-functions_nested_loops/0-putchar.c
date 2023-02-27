68d5215d1a1969f8cfdb44ce241940c039d0ef76
MaxwelKim <mwiti.kimathi1796@gmail.com>
2023-02-27 13:24:42 +0300

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,22 @@
 #include "main.h"
-#include "stdio.h"
+
 /**
- * _putchar - print _putchar, followed by new line
- * Return: Always 0
- */
-int main(void);
+*main - prints the word Holberton
+*
+*Return: 0 on success execution.
+*/
+
+int main(void)
 {
-{
-char (_, p, u, t, c, h, e, r);
-}
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
-return (0);
+	char s[] = "_putchar";
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		_putchar(s[i]);
+		i++;
+	}
+	_putchar(10);
+
+	return (0);
 }
