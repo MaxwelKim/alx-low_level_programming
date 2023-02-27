a220ef7c60f498ef53ee0357012288c94648be3b
MaxwelKim <mwiti.kimathi1796@gmail.com>
2023-02-27 13:45:05 +0300

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,18 +4,17 @@
  * main - Prints _putchar as a message and then a newline.
  * Return: Always 0 (Success)
 */
-int main(void);
+int main(void); {
 {
-{_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 }
 return (0);
-	}
 }
