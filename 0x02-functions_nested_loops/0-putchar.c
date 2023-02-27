1ee1a43aaa642e3387c8e9a86224d45ba9ece562
MaxwelKim <mwiti.kimathi1796@gmail.com>
2023-02-28 00:08:44 +0300

    program that prints _putchar, followed by a new line

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,19 @@
 #include "main.h"
 
 /**
-*main - prints the word Holberton
-*
-*Return: 0 on success execution.
-*/
+ * main - Entry point
+ * Return:0
+ */
 
 int main(void)
 {
-	char s[] = "_putchar";
-	int i = 0;
-
-	while (s[i] != '\0')
-	{
-		_putchar(s[i]);
-		i++;
-	}
-	_putchar(10);
-
-	return (0);
+char c[9] = "_putchar";
+int i = 0;
+while (i < 8)
+{
+_putchar(c[i]);
+i++;
+}
+_putchar('\n');
+return (0);
 }
