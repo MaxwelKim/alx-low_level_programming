a962f890b6c1df816a70cbbc68ea557ae98c1aad
MaxwelKim <mwiti.kimathi1796@gmail.com>
2023-03-26 22:28:48 +0300

    prototype functions

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,7 +1,20 @@
-#ifndef VARDIC_FUNCTIONS
-#define VARDIC_FUNCTION
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+#include <stdarg.h>
+#include <stdio.h>
+int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-#endif /* VARDIC_FUNCTIONS*/
+/**
+ * struct print - A new struct type defining our print
+ * @symbol: Our symbol representing the data type
+ * @print_func: a function pointer that prints
+ **/
+typedef struct print
+{
+char *symbol;
+void (*print_func)(va_list arg);
+} func_printer;
+#endif /** VARIADIC_FUNCTIONS_H */
